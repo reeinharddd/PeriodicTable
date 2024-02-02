aad0fa32dc8d33505738bd9d1813d367d66ec8c1
erik <beltraberik@gmail.com>
2024-02-02 14:10:29 -0800

    Se agrego la logica para cambiar el color de la celda dependiendo de que tipo de elemento es

--- index.php
+++ index.php
@@ -12,54 +12,55 @@
     <!-- <script type = "module" src = "https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
 <model-viewer src="https://storage.googleapis.com/search-ar-edu/periodic-table/element_009_fluorine/element_009_fluorine.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls tone-mapping="commerce" poster="poster.webp" shadow-intensity="1" autoplay> </model-viewer>     -->
     <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-$url = 'https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json';
+    $url = 'https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json';
 
-$data = file_get_contents($url);
-$array = json_decode($data, true);
+    $data = file_get_contents($url);
+    $array = json_decode($data, true);
 
-if (!$array) {
-    echo "No hay datos o hay un error en la decodificación del JSON.";
-} else {
-    echo "<div class ='periodic'>";
+    if (!$array) {
+        echo "No hay datos o hay un error en la decodificación del JSON.";
+    } else {
+        echo "<div class ='periodic'>";
 
-    $rows = 7;
-    $cols = 18; 
+        $rows = 7;
+        $cols = 18;
 
-    for ($row = 1; $row <= $rows; $row++) {
-        echo "<div class='periodic-row'>";
+        for ($row = 1; $row <= $rows; $row++) {
+            echo "<div class='periodic-row'>";
 
-        for ($col = 1; $col <= $cols; $col++) {
-            $index = ($row - 1) * $cols + $col - 1;
+            for ($col = 1; $col <= $cols; $col++) {
+                $index = ($row - 1) * $cols + $col - 1;
 
-            if (isset($array['elements'][$index])) {
-                $element = $array['elements'][$index];
+                if (isset($array['elements'][$index])) {
+                    $element = $array['elements'][$index];
 
-                echo "<a href='element.php?elemento=" . $element['name'] . "'>";
-                                echo "<div class='cell'>";
+                    echo "<a href='element.php?elemento=" . $element['name'] . "'>";
+                    echo "<div class='cell'>";
+                    $category = str_replace(' ', '_', $element["category"]);
 
-                echo "<div class='element'>";
-                echo "<div class='at_num'>" . $element['number'] . "</div>";
-                echo "<div class='symbol'>" . $element['symbol'] . "</div>";
-                echo "<div class='at_details'>" . $element['name'] . "<br />" . $element['atomic_mass'] . "</div>";
-                echo "</div>";
-                                echo "</div>";
+                    echo "<div class='element " . $category . "'>";
+                    echo "<div class='at_num'>" . $element['number'] . "</div>";
+                    echo "<div class='symbol'>" . $element['symbol'] . "</div>";
+                    echo "<div class='at_details'>" . $element['name'] . "<br />" . $element['atomic_mass'] . "</div>";
+                    echo "</div>";
+                    echo "</div>";
 
-                echo "</a>";
-            } else {
-                echo "<div class='cell'></div>";
+                    echo "</a>";
+                } else {
+                    echo "<div class='cell'></div>";
+                }
             }
+
+            echo "</div>";
         }
 
         echo "</div>";
     }
-
-    echo "</div>";
-}
-?>
+    ?>
 
 </body>
 
