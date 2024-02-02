e1c9fca0ded1158e4c4600a2e1f4d616fcce4bca
erik <beltraberik@gmail.com>
2024-02-02 15:47:49 -0800

    testos

--- element.php
+++ element.php
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="test.css">
+
 </head>
 
 <body>
-<header>
+    <header>
 
-</header>
+    </header>
 
     <?php
     if (isset($_GET['elemento'])) {
@@ -20,18 +22,42 @@
         $data = file_get_contents($url);
         $array = json_decode($data, true);
 
-
+        echo "<div class='container'>";
+        echo "<div class='grid'>";
         if (!$array) {
             echo "No hay datos o hay un error en la decodificación del JSON.";
         } else {
-            foreach ($array['elements'] as $elements) {
-                if ($elements['name'] === $elementName) {
-                    foreach ($elements as $key => $element) {
-                        echo "$key: $element<br>";
-                    }
+            foreach ($array['elements'] as $element) {
+                if ($element['name'] === $elementName) {
+                    echo '' . $elements['name'] . '';
+
+                    echo "<div class='item-b'>
+                       <p>
+                        " . $element['category'] . "
+                        </p>
+                        </div>";
+                    echo "<div class='item-b'></div>";
+                    echo "<div class='item-c'></div>";
+
+                    echo "<div class='item-d'></div>";
+
+                    echo "<div class='item-d'></div>";
+
+                    echo "<div class='item-e'></div>";
+
+                    echo "<div class='item-f'></div>";
+
+                    echo "<div class='item-g'></div>";
+
+                    echo "<div class='item-h'></div>";
+
+                    echo "<div class='item-i'></div>";
+                    echo "<div class='item-j'></div>";
                 }
             }
         }
+        echo "</div>";
+        echo "</div>";
     }
 
     ?>
