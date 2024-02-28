5f857f778097c37e960bd05f4c8e37b1767d4f1b
Emma <m@gmail.com>
2024-02-28 11:44:25 -0800

    Estilos de elementos

--- element.php
+++ element.php
@@ -4,14 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php $elementName ?></title>
+    <title>Element Information</title>
     <link rel="stylesheet" href="element.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+</head>
 
 </head>
 <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
 
 <body>
     <header>
+
+        <a href="index.php" id="icon"><img src="img/atras.png" alt="Back icon"> Back</a> <!-- Back button -->
+        <a href="#basicInfo" id="icon"> <img src="img/iconColor1.png" alt="Info icon"> Basic Information</a> <!-- Basic Information / item b-->
+        <a href="#summary" id="icon"><img src="img/iconColor1.png" alt="Summary icon"> Summary</a> <!-- Summary / item c-->
+        <a href="#physical" id="icon"><img src="img/iconColor1.png" alt="Physical icon"> Physical</a> <!-- Physical / item d-->
+        <a href="#chemical" id="icon"><img src="img/iconColor1.png" alt="Chemical icon"> Chemical</a> <!-- Chemical / item e-->
+        <a href="#discovery" id="icon"><img src="img/iconColor1.png" alt="Discovery icon"> Discovery and Named</a> <!-- Discovery and Named / item f-->
+        <a href="#position" id="icon"><img src="img/iconColor1.png" alt="Position icon"> Position in the Periodic Table</a> <!-- Position in the Periodic Table / item g-->
+        <a href="#image" id="icon"><img src="img/iconColor1.png" alt="Image icon"> Image</a> <!-- Image / item h-->
+        <a href="#electronic" id="icon"><img src="img/iconColor1.png" alt="Electronic icon"> Electronic Configuration</a> <!-- Electronic Configuration / item i-->
 
     </header>
 
@@ -25,105 +37,209 @@
 
         echo "<div class='container'>";
         echo "<div class='grid'>";
-        
+
         if (!$array) {
             echo "No hay datos o hay un error en la decodificación del JSON.";
         } else {
             foreach ($array['elements'] as $element) {
                 if ($element['name'] === $elementName) {
                     echo "<div class='item-a item'>";
-                    echo "1";
-                    echo "<h1>" . $element['name'] . "</h1>";
-                    echo "<model-viewer src='".$element['bohr_model_3d']."' ar ar-modes='webxr scene-viewer quick-look' camera-controls tone-mapping='commerce' poster='poster.webp' shadow-intensity='2' autoplay> </model-viewer> ";
+                    echo "<div class='title-icon'><h1>" . $element['number'] . "</h1></div>";
+                    echo "<div class='title-symbol'><h1>" . $element['symbol'] . "</h1></div>";
+                    echo "<div class='title-text'><h1>" . $element['name'] . "</h1></div>";
+                    echo "<div class='title-mass'><h1>" . $element['atomic_mass'] . "</h1></div>";
+                    
+                    //echo "<model-viewer src='" . $element['bohr_model_3d'] . "' ar ar-modes='webxr scene-viewer quick-look' camera-controls tone-mapping='commerce' poster='poster.webp' shadow-intensity='2' autoplay> </model-viewer> ";
                     echo "</div>";
 
-                    // Basic information of the element
-                    echo "<div class='item-b item'>";
-                    echo "<h1 id='title'>Basic information</h1>";;
-                    echo "<p><strong>Name::</strong> " . $element['name'] . "</p>";
-                    echo "<p><strong>Atomic Number:</strong> " . $element['number'] . "</p>";
-                    echo "<p><strong>Atomic Mass:</strong> " . $element['atomic_mass'] . "</p>";
-                    echo "<p><strong>Appearance:</strong> " . $element['appearance'] . "</p>";
-                    echo "<p><strong>Category:</strong> " . $element['category'] . "</p>";
-                    echo "<p><strong>Period:</strong> " . $element['period'] . "</p>";
-                    echo "<p><strong>Group:</strong> " . $element['xpos'] . "</p>";
-                    echo "</div>";
+    ?>
+                    <div class='item-b item' id='basicInfo'>
+                        
+                            <h1 id='title'>Basic information</h1>
 
-                    // Phase of the element
-                    echo "<div class='item-c item'?";
-                    echo "3";
+                        <table>
+                            <tr>
+                                <td><strong>Name:</strong></td>
+                                <td><?php echo $element['name']; ?></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Atomic Number:</strong></td>
+                                <td><?php echo $element['number']; ?></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Atomic Mass:</strong></td>
+                                <td><?php echo $element['atomic_mass']; ?></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Appearance:</strong></td>
+                                <td><?php echo $element['appearance']; ?></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Category:</strong></td>
+                                <td><?php echo $element['category']; ?></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Period:</strong></td>
+                                <td><?php echo $element['period']; ?></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Group:</strong></td>
+                                <td><?php echo $element['xpos']; ?></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <?php
+                    echo "<div class='item-c item' id='summary'>";
+                    // echo "3"; 
+                    echo "<h1>Summary</h1>";
                     echo "<p><strong>Phase:</strong> " . $element['summary'] . "</p>";
                     echo "</div>";
+                    ?>
+                    <div class='item-d item' id='physical'>
+                        <div class='title-container'>
+                            <h1>Physical</h1>
+                        </div>
+                        <div class="scroll">
+                            <table>
+                                <tr>
+                                    <td><strong>Apparience:</strong></td>
+                                    <td><?php echo $element['appearance']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Density:</strong></td>
+                                    <td><?php echo $element['density']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Phase:</strong></td>
+                                    <td><?php echo $element['phase']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Boiling Point:</strong></td>
+                                    <td><?php echo $element['boil']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Melting Point:</strong></td>
+                                    <td><?php echo $element['melt']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Electron Affinity:</strong></td>
+                                    <td><?php echo $element['electron_affinity']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Ionization Energy:</strong></td>
+                                    <td><?php echo $element['molar_heat']; ?></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
 
-                    // Chemical properties of the element
-                    echo "<div class='item-d item'>";
-                    echo "4";
-                    echo "<h1 id='title'>Physical</h1>";
-                    echo "<p><strong>Apparience:</strong> " . $element['appearance'] . "</p>";
-                    echo "<p><strong>Density:</strong> " . $element['density'] . "</p>";
-                    echo "<p><strong>Phase:</strong> " . $element['phase'] . "</p>";
-                    echo "<p><strong>Boiling Poin:</strong> " . $element['boil'] . "</p>";
-                    echo "<p><strong>Melting Point:</strong> " . $element['melt'] . "</p>";
-                    echo "<p><strong>Electron Affinity:</strong> " . $element['electron_affinity'] . "</p>";
-                    echo "<p><strong>Ionization Energy:</strong> " . $element['molar_heat'] . "</p>";
-                    echo "</div>";  
+                    <div class='item-e item' id='chemical'>
+                        <div class='title-container'>
+                            <h1>Chemical</h1>
+                        </div>
+                        <div class="scroll">
+                            <table>
+                                <tr>
+                                    <td><strong>Atomic Mass:</strong></td>
+                                    <td><?php echo $element['atomic_mass']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Electron Configuration:</strong></td>
+                                    <td><?php echo $element['electron_configuration']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Semantic Electron:</strong></td>
+                                    <td><?php echo $element['electron_configuration_semantic']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Electron Affinity:</strong></td>
+                                    <td><?php echo $element['electron_affinity']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Electronegativity:</strong></td>
+                                    <td><?php echo $element['electronegativity_pauling']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Ionization Energy:</strong></td>
+                                    <td>
+                                        <div class="scroll">
+                                            <table>
+                                                <?php
+                                                foreach ($element['ionization_energies'] as $energy) {
+                                                    echo "<tr><td>$energy</td></tr>";
+                                                }
+                                                ?>
+                                            </table>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
 
-                    echo "<div class='item-e item'>";
-                    echo "5";
-                    echo "<h1 id='title'>Chemical</h1>";
-                    echo "<p><strong>Atomic Mass:</strong> " . $element['atomic_mass'] . "</p>";
-                    echo "<p><strong>Electron Configuration:</strong> " . $element['electron_configuration'] . "</p>";
-                    echo "<p><strong>Semantic Electron:</strong> " . $element['electron_configuration_semantic'] . "</p>";
-                    echo "<p><strong>Electron Affinity:</strong> " . $element['electron_affinity'] . "</p>";
-                    echo "<p><strong>Electronegativity:</strong> " . $element['electronegativity_pauling'] . "</p>";
-                    echo "<p><strong>Ionization Energy:</strong> ";
-                    foreach ($element['ionization_energies'] as $energy) {
-                        echo $energy . ", ";
-                    }
-                    echo "</p>";
-                    echo "</div>"; // End of the grid
+                    <div class='item-f item' id='discovery'>
+                        <div class='title-container'>
+                            <h1>Discovery and Named</h1>
+                        </div>
+                        <div class="scroll2">
+                            <table>
+                                <tr>
+                                    <td><strong>Discovered by:</strong></td>
+                                    <td><?php echo $element['discovered_by']; ?></td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Named by:</strong></td>
+                                    <td><?php echo $element['named_by']; ?></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
 
-                    echo "<div class='item-f item'>";
-                    echo "6";
-                    echo "<h1 id='title'>Discovery and Named</h1>";
-                    echo "<p><strong>Discovered by:</strong> " . $element['discovered_by'] . "</p>";
-                    echo "<p><strong>Named by:</strong> " . $element['named_by'] . "</p>";
-                    echo "</div>";
+                    <div class='item-g item' id='position'>
+                        <div class='title-container'>
+                            <h1>Position in the Periodic Table</h1>
+                        </div>
+                        <table>
+                            <tr>
+                                <td><strong>X position:</strong></td>
+                                <td><?php echo $element['xpos']; ?></td>
+                            </tr>
+                            <tr>
+                                <td><strong>Y position:</strong></td>
+                                <td><?php echo $element['ypos']; ?></td>
+                            </tr>
+                        </table>
+                    </div>
 
-                    echo "<div class='item-g item'>";
-                    echo "7"; 
-                    echo "<h1 id='title'>Position in the Periodic Table</h1>";
-                    echo "<p><strong>X position:</strong> " . $element['xpos'] . "</p>";
-                    echo "<p><strong>Y position:</strong> " . $element['ypos'] . "</p>";
-                    echo"</div>";
+                    <div class='item-h item' id='image'>
+                        <div class='title-container'>
+                        </div>
+                        <img src='<?php echo $element['image']['url']; ?>' alt='Image of <?php echo $element['name']; ?>'>
+                    </div>
 
-                    // Image of the element
-                    echo "<div class='item-h item'>";
-                    echo "8";
-                    echo "<h1 id='title'>Image</h1>";
-                    // muestra el nombre y url de la imagen
-                    echo "<img src='" . $element['image']['url'] . "' alt='Image of " . $element['name'] . "'>";
-                    echo "</div>";
-
-                    // Electronic configuration of the element
-                    echo "<div class='item-i item'>";
-                    echo "<h1 id='title'>Electronic Configuration</h1>";
-                    foreach ($element['shells'] as $shell) {
-                        echo "<p><strong>Shell:</strong> " . $shell . "</p>";
-                    }
-                    echo "</div>";
+                    <div class='item-i item' id='electronic'>
+                        <div class='title-container'>
+                            <h1>Electronic Configuration</h1>
+                        </div>
+                        <div class="scroll3">
+                            <table>
+                                <?php
+                                foreach ($element['shells'] as $shell) {
+                                    echo "<tr><td><strong>Shell:</strong></td><td>$shell</td></tr>";
+                                }
+                                ?>
+                            </table>
+                        </div>
+                    </div>
 
 
+    <?php
                 }
             }
         }
-        echo "</div>";
-        echo "</div>";
     }
 
+
+    echo "</div>";
+    echo "</div>";
     ?>
-
-    </div>
-</body>
-
-</html>
